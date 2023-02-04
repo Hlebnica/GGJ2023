4a63dc8dfd95176d31bfe27d1c38c16bfe6648a6
Catlerc <mcr1123@yandex.ru>
2023-02-05 00:26:36 +0500

    level 1 steam machine anim

--- Assets/Scripts/Level1/Logic.cs
+++ Assets/Scripts/Level1/Logic.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,9 +8,12 @@ public class Logic : MonoBehaviour
     public Holler a;
     public Holler b;
     public Holler c;
+    public GameObject dim;
 
     public int s = 0;
 
+    public Transform[] rot;
+    public Transform[] arot;
     public void inc()
     {
         s++;
@@ -22,9 +26,38 @@ public class Logic : MonoBehaviour
             case 2:
                 break;
             case 3:
-                SceneManager.LoadScene("Level2");
+                StartCoroutine(Anim());
                 break;
         }
+    }
+
+    IEnumerator Anim()
+    {
+        dim.SetActive(true);
+        float startTime = Time.time;
+
+        while (true)
+        {
+
+            foreach (var r in rot)
+            {
+                var rr=r.rotation;
+                rr.eulerAngles = Vector3.right * Time.time * 40;
+                r.rotation = rr;
+            }
+            foreach (var r in arot)
+            {
+                var rr=r.rotation;
+                rr.eulerAngles = Vector3.right * Time.time * -40;
+                r.rotation = rr;
+            }
+            
+            if (Time.time - startTime > 5)
+                SceneManager.LoadScene("Level2");
+            yield return null;
+        }
+
+        
     }
 
 }
