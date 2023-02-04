af2b11502d29fb1a5897270d6eac5438868ddac8
Catlerc <mcr1123@yandex.ru>
2023-02-04 23:26:31 +0500

    bat anim

---
+++ Assets/Scripts/Level2/BatFly.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class BatFly : MonoBehaviour
+{
+    public float offset;
+
+    private void Update()
+    {
+        var pos = transform.position;
+        pos.y += Mathf.Sin(Time.time*2+offset)*Time.deltaTime;
+        pos.x += Mathf.Cos(Time.time*2+offset)/1.3f*Time.deltaTime;
+        transform.position = pos;
+    }
+
+}
--- Assets/Scripts/Level2/HollerCrystal.cs
+++ Assets/Scripts/Level2/HollerCrystal.cs
@@ -15,22 +15,39 @@ public class HollerCrystal : MonoBehaviour
 
     public GameObject Blocker;
 
+    public GameObject[] bats;
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("ENTER "+other.transform.parent.gameObject.name);
-        Debug.Log("KEK "+target.gameObject.name);
+        // Debug.Log("ENTER "+other.transform.parent.gameObject.name);
+        // Debug.Log("KEK "+target.gameObject.name);
         if (other.transform.parent.gameObject.name == target.gameObject.name && !done)
         {
-            
-            _event.Invoke();;
-            
+            _event.Invoke();
+
             other.tag = "Untagged";
             done = true;
             Destroy(Blocker);
             Destroy(GetComponent<BoxCollider>());
             ore.constraints = RigidbodyConstraints.None;
 
+            StartCoroutine(FlyAnim());
+        }
+    }
 
+    IEnumerator FlyAnim()
+    {
+        float startTime = Time.time;
+        Debug.Log("START");
+        while (startTime+3  > Time.time)
+        {
+            Debug.Log("!!!!!!!!@#");
+            foreach (var bat in bats)
+            {
+                var pos = bat.transform.position;
+                bat.transform.position = pos+new Vector3(0.1f,6,0)*Time.deltaTime;
+            }
+            yield return null;
         }
     }
 }
